4a5785383c6393c84be0cb1fdd12a97f9e0e2936
abbos <qobiljonovv102@mail.ru>
2021-10-11 17:16:52 +0500

    methodlani qoshib qoydim

--- register_test.py
+++ register_test.py
@@ -3,5 +3,38 @@ import platform
 import sys
 import mysql.connector
 
+def clear_everything():
+    if platform.system() == "Linux":
+        os.system("clear")
+    elif platform.system() == "Windows":
+        os.system("cls")
+    else:
+        print("Sorry we do not now this system")
 
+class Register:
+    def __init__(self):
+        pass
+
+    def menu(self):
+        pass
+
+    def choosed(self):
+        pass
+
+    def register(self):
+        pass
+    def log_in(self):
+        pass
+    def update_password(self):
+        pass
+    def update_login(self):
+        pass
+    def delete_account(self):
+        pass
+    def error(self):
+        pass
+    def back(self):
+        pass
+
+    
 
