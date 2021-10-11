f4b62905e471f2541e99a49c393614ce7d02570b
abbos <qobiljonovv102@mail.ru>
2021-10-11 17:47:52 +0500

    databasega ulandi

--- register_test.py
+++ register_test.py
@@ -11,15 +11,42 @@ def clear_everything():
     else:
         print("Sorry we do not now this system")
 
+my_db = mysql.connector.connect(
+    host="localhost",
+    user="abbos",
+    password="12345678",
+    database="dang"
+)
+my_cursor = my_db.cursor()
+my_cursor.execute("create table if not exists login_passowrd(id int(6) unsigned auto_increment primary key, name varchar(20), surname varchar(20), login varchar(20), password varchar(20))")
+my_db.commit()
+
+
 class Register:
     def __init__(self):
-        pass
+        self.menu()
 
     def menu(self):
-        pass
+        answer = input("""
+What do you want
+Please choose one of them
+        Sign in [1]
+        Log in  [2]
+        Exit    [3]   
+:""")
+        self.choosed(answer)
 
-    def choosed(self):
-        pass
+    def choosed(self, chose):
+        if chose == '1':
+            clear_everything()
+            self.register()
+        elif chose == '2':
+            clear_everything()
+            self.log_in()
+        else:
+            print("Bye ;)")
+            exit()
+
 
     def register(self):
         pass
@@ -36,5 +63,7 @@ class Register:
     def back(self):
         pass
 
-    
+
+
+user1 = Register()
 
